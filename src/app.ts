62a249a74932556f29c153ff76e14085c7775a80
cardenaz <william@cardenas>
2022-10-08 15:44:27 +0200

    yes

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'; 
+
+const app = express(); 
+
+app.get('/', (req, res) => {
+    return res.send('Geotracker')
+}); 
+
+app.listen(3000, () => {
+    console.log('App listening at http://localhost:3000')
+
+}); 
