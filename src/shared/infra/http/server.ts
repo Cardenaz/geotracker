12293fb5630d2fbba0f040d16186e52a3eae26d5
cardenaz <william@cardenas.se>
2022-10-15 20:56:47 +0200

    first step

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,5 @@
 import { createServer } from "http";
-import { Server} from "socket.io";
+import { Server } from "socket.io";
 import { app } from "./app";
 
 const port = process.env.PORT || 3000; 
@@ -11,7 +11,20 @@ const io = new Server(httpServer, {
   });
 io.on('connection', (socket) => {
   console.log('User connected'); 
+
+  socket.on("position-change", (data) => {
+    console.log(data); 
+    io.emit("position-change", data); 
+  }); 
+  socket.on('disconnect', () => {
+    console.log("User disconnected"); 
+  }
+  ); 
 }); 
+
+
+
+
 httpServer.listen(port, () => {
   console.log(`Listening on ${port}`)
 });
---
+++ src/shared/infra/socketIO/index.ts
@@ -0,0 +1,5 @@
+import { SocketIORemoteDataSourceImpl } from "./socketRemoteDataSource";
+
+const socketImpl = new SocketIORemoteDataSourceImpl(); 
+
+export { socketImpl }; 
