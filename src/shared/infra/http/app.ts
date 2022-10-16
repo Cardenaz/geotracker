bd4b6cf56b852994ec6a0f10682ddf6c1849c59f
cardenaz <william@cardenas.se>
2022-10-16 11:35:09 +0200

    helmet middleware

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import helmet from 'helmet'; 
 import {v1Router} from './api/v1'; 
 
 const app = express(); 
 
+app.use(helmet())
 app.use(v1Router); 
 
 export {app}; 
